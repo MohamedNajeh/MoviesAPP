4c22790664d52e009af06cc4dea1b7bbad029833
MohamedNajeh <mohamed.najeh.ahmed@gmail.com>
2022-03-06 00:40:59 +0200

    first commit

---
+++ MoviesApp/CollectionViewItem.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewItem.swift
+//  MoviesApp
+//
+//  Created by Najeh on 07/02/2022.
+//
+
+import UIKit
+
+class CollectionViewItem: UICollectionViewCell {
+    
+}
---
+++ MoviesApp/MovieDetailsVC.swift
@@ -0,0 +1,29 @@
+//
+//  MovieDetailsVC.swift
+//  MoviesApp
+//
+//  Created by Najeh on 04/02/2022.
+//
+
+import UIKit
+
+class MovieDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
